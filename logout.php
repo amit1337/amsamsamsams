495b2a0103f8767c24b2de1eb6e10394068d5eb2
amit1337 <amit@thoughtfulviewfinder.in>
2016-04-27 14:39:50 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -3,3 +3,119 @@ session_start();
 session_destroy();
 header('location:login.php');
 ?>
+
+
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script> 
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+
+
+
+
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script>
+    function insert()
+{
+
+
+var name = $("#name").val();
+var email = $("#email").val();
+var password = $("#password").val();
+var contact = $("#contact").val();
+// Returns successful data submission message when the entered information is stored in database.
+var dataString = 'name1='+ name + '&email1='+ email + '&password1='+ password + '&contact1='+ contact;
+
+
+
+
+
+
+$.ajax({
+   
+   type:'POST',
+	url:'ajax.php',
+	data:dataString,
+	success:function(reply)
+	{
+		$('#result').html(reply);
+	}
+})
+}
+
+</script>
+
+
+
+
+
+
+<!-- Button trigger modal -->
+&nbsp<br><br><button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+  Add INFO
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+      </div>
+
+
+      <div class="modal-body">
+
+
+        <div class="form-group">
+                  <label for="exampleInputEmail1">Name</label>
+                  <input type="text" class="form-control" name="name" id="name">
+                </div>
+
+
+<div class="form-group">
+                  <label for="exampleInputEmail1">Phone</label>
+                  <input type="text" class="form-control" name="email">
+                </div>
+
+
+<div class="form-group">
+                  <label for="exampleInputEmail1">Email</label>
+                  <input type="text" class="form-control" name="password">
+                </div>
+
+<div class="form-group">
+                  <label for="exampleInputEmail1">Address</label>
+                  <input type="text" class="form-control"  name="contact">
+                </div>
+
+<input type="submit" class="btn btn-primary" onclick="insert();" >
+
+
+
+
+<div id="result"></div>
+
+
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        
+      </div>
+    </div>
+  </div>
+</div>
